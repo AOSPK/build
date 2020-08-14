ViperOS/android_build

build: Fix name paths for our project
Signed-off-by: Alexandre Rangel <mamutal91@gmail.com>

core/main.mk
@@ -364,7 +364,7 @@ ADDITIONAL_BUILD_PROPERTIES += net.bt.name=Android
 
 # ------------------------------------------------------------
 # Include vendor specific additions to build properties
--include vendor/lineage/build/core/main.mk
+-include vendor/viper/build/core/main.mk
 
 # ------------------------------------------------------------
 # Define a function that, given a list of module tags, returns

AOSPK/build

core/main.mk
@@ -373,7 +373,7 @@ ADDITIONAL_BUILD_PROPERTIES += net.bt.name=Android
 
 # ------------------------------------------------------------
 # Include vendor specific additions to build properties
--include vendor/lineage/build/core/main.mk
+-include vendor/aosp/build/core/main.mk
 
 # ------------------------------------------------------------
 # Define a function that, given a list of module tags, returns