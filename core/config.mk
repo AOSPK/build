995cb15582a766d5fb69db65c4ba2f70ef59ddef
Alexandre Rangel <mamutal91@gmail.com>
2020-10-31 17:44:56 -0300

    build: Fix name paths for our project

--- core/config.mk
+++ core/config.mk
@@ -296,7 +296,7 @@ FIND_LEAVES_EXCLUDES := $(addprefix --prune=, $(SCAN_EXCLUDE_DIRS) .repo .git)
 
 -include vendor/extra/BoardConfigExtra.mk
 ifneq ($(LINEAGE_BUILD),)
-include vendor/lineage/config/BoardConfigLineage.mk
+include vendor/aosp/config/BoardConfig.mk
 endif
 
 # The build system exposes several variables for where to find the kernel
@@ -1234,10 +1234,10 @@ dont_bother_goals := out \
     product-graph dump-products
 
 ifneq ($(LINEAGE_BUILD),)
-ifneq ($(wildcard device/lineage/sepolicy/common/sepolicy.mk),)
+ifneq ($(wildcard device/custom/sepolicy/common/sepolicy.mk),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
-$(eval include device/lineage/sepolicy/common/sepolicy.mk)
+$(eval include device/custom/sepolicy/common/sepolicy.mk)
 endif
 endif
 
